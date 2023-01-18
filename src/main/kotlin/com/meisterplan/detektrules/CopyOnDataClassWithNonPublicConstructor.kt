5b4a700318777b411aebffe4cec7a7f7b2a5abef
Tobias Kuhn <tobias.kuhn@meisterplan.com>
2023-01-18 13:39:20 +0100

    Implement CopyOnDataClassWithNonPublicConstructor

---
+++ src/main/kotlin/com/meisterplan/detektrules/CopyOnDataClassWithNonPublicConstructor.kt
@@ -0,0 +1,46 @@
+package com.meisterplan.detektrules
+
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
+import io.gitlab.arturbosch.detekt.rules.fqNameOrNull
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.psi.psiUtil.getReceiverExpression
+import org.jetbrains.kotlin.resolve.lazy.descriptors.LazyClassMemberScope
+
+class CopyOnDataClassWithNonPublicConstructor(config: Config) : Rule(config) {
+    override val issue = Issue(
+        javaClass.simpleName,
+        Severity.CodeSmell,
+        "This rule reports calls of copy() on a data class that has a non-private constructor. This can lead to correctness issues, since copy()" +
+                " is always public and allows to bypass the constructor.",
+        Debt.TEN_MINS,
+    )
+
+    override fun visitCallExpression(expression: KtCallExpression) {
+        super.visitCallExpression(expression)
+        val context = expression.context
+        val calleeExpression = expression.calleeExpression
+        if (context is KtDotQualifiedExpression
+            && calleeExpression is KtNameReferenceExpression
+            && calleeExpression.getReferencedName().lowercase() == "copy"
+        ) {
+            val type = bindingContext.getType(expression)
+            val typeMemberScope = type?.memberScope
+            if (typeMemberScope is LazyClassMemberScope
+                && typeMemberScope.getPrimaryConstructor()?.visibility != DescriptorVisibilities.PUBLIC
+            )
+                report(CodeSmell(issue, Entity.from(expression), "Non-public constructed data class ${calleeExpression.getReceiverExpression()?.text ?: ""}"
+                        + " of type ${type.fqNameOrNull()} should not bypass constructor by calling copy()."))
+        }
+
+    }
+}
