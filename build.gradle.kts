5f8e02fa5aa2bc6523a90fe6e760c66d179b686b
GitHub Action <action@github.com>
2023-01-18 12:22:24 +0000

    Change package name from org.example.detekt to com.github.meisterplan.detektrules

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     `maven-publish`
 }
 
-group = "org.example.detekt"
+group = "com.github.meisterplan.detektrules"
 version = "1.0-SNAPSHOT"
 
 repositories {
--- src/main/kotlin/com/github/meisterplan/detektrules/MyRule.kt
+++ src/main/kotlin/com/github/meisterplan/detektrules/MyRule.kt
@@ -1,4 +1,4 @@
-package org.example.detekt
+package com.github.meisterplan.detektrules
 
 import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
